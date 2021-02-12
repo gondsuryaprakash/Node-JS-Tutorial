0f89c643f95ef63e4c24e88f0515692855f04f36
gondsuryaprakash <suryaprakashgond15243@gmail.com>
2021-02-12 20:37:54 +0530

    Add all

---
+++ rough.js
@@ -0,0 +1,21 @@
+
+console.log(__dirname)
+console.log(__filename)
+var buff=new Buffer(10)
+console.log(buff)
+var a= new Buffer([10, 20, 30, 40, 50]);
+console.log(a)
+const os=require('os');  
+console.log("os.freemem(): \n",os.freemem());  
+console.log("os.homedir(): \n",os.homedir());  
+console.log("os.hostname(): \n",os.hostname());  
+console.log("os.endianness(): \n",os.endianness());  
+console.log("os.loadavg(): \n",os.loadavg());  
+console.log("os.platform(): \n",os.platform());  
+console.log("os.release(): \n",os.release());  
+console.log("os.tmpdir(): \n",os.tmpdir());  
+console.log("os.totalmem(): \n",os.totalmem());  
+console.log("os.type(): \n",os.type());  
+console.log("os.uptime(): \n",os.uptime()); 
+
+
